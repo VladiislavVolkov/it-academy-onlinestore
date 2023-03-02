10ecc85731b2a11ab7112405b330694c0c278819
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-02 22:13:49 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
 import { Component } from './core/Component';
+import './components/organisms/Header';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
-
-  increaseCount() {
-    this.setState((state) => {
-      return {
-        count: state.count < 10 ? state.count + 1 : state.count,
-      };
-    });
-  }
-
-  decreaseCount() {
-    this.setState((state) => {
-      return {
-        count: state.count ? state.count - 1 : state.count,
-      };
-    });
-  }
-
-  onClick = (evt) => {
-    if (evt.target.closest('.plus')) {
-      this.increaseCount();
-    }
-    if (evt.target.closest('.minus')) {
-      this.decreaseCount();
-    }
-  };
-
-  componentDidMount() {
-    this.addEventListener('click', this.onClick);
-  }
-
   render() {
     console.log(this.props);
     return `
-        <div> 
-            <h1>APP - OnLine Shop</h1>
-            <button class='btn btn-primary minus'> - </button>
-            <span>${this.state.count}</span>
-            <button class='btn btn-primary plus'> + </button>
-        </div>
+        <it-header></it-header>
     `;
   }
 }
+
 customElements.define('it-app', App);

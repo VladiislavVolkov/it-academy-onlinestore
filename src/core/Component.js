9b5a158791067f7c0faeae0be43226ead16ba4b0
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-19 14:04:20 +0300

    new

---
+++ src/core/Component.js
@@ -0,0 +1,43 @@
+export class Component extends HTMLElement {
+  constructor() {
+    super();
+    this.props = {};
+    this.state = {};
+  }
+
+  setState(callback) {
+    this.state = callback();
+    this.innerHTML = this.render();
+  }
+
+  connectedCallback() {
+    this.componentDidMount();
+    this.innerHTML = this.render();
+  }
+
+  disconnectedCallback() {
+    this.componentWillUnmount();
+  }
+
+  // метод вызван тогдаб, когда атрибуты нашего компонента будут изменены
+  // name - имя атрибута
+  // oldValue - старое значение атрибута
+  // newValue - новое значение атрибута
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.componentWillUpdate(name, oldValue, newValue);
+    // поведение которое следит за всеми атрибутами
+    this.getAttributeNames().forEach((attributeName) => {
+      this.props[attributeName] = this.getAttribute(attributeName);
+    });
+  }
+
+  // метод при встраивании в дерево
+  componentDidMount() {}
+
+  // метод когда удаляемся из дерева
+  componentWillUnmount() {}
+
+  componentWillUpdate() {}
+
+  render() {}
+}

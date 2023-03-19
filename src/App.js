4ac49844cee7481d0cb624bceefed44561042dfd
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-19 13:39:53 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 class App extends HTMLElement {
   constructor() {
+    super();
     this.state = {
       count: 0,
     };
@@ -7,22 +8,40 @@ class App extends HTMLElement {
 
   increaseCount = () => {
     this.state.count = this.state.count + 1;
+    console.log(this.state);
+    this.innerHTML = this.render();
   };
 
   decreaseCount = () => {
     this.state.count = this.state.count - 1;
+    console.log(this.state);
+    this.innerHTML = this.render();
+  };
+
+  onClick = (evt) => {
+    if (evt.target.closest('.plus')) {
+      this.increaseCount();
+    }
+    if (evt.target.closest('.minus')) {
+      this.decreaseCount();
+    }
   };
 
   connectedCallback() {
     this.innerHTML = this.render();
+    this.addEventListener('click', this.onClick);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this.onClick);
   }
 
   render() {
     return `
         <div>
-            <button class='btn'>---</button>
+            <button class='btn btn-primary minus'>---</button>
             <span>${this.state.count}</span>
-            <button class='btn'>+++</button>
+            <button class='btn btn-primary plus'>+++</button>
         </div>
     `;
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './main.scss';
 import './App';
 
-const root = document.querySelector('root');
+const root = document.getElementById('root');
 const app = document.createElement('it-app');
 
 root.append(app);

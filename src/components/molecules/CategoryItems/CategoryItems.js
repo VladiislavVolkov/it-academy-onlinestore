a5d98b5f56805ae3b51204b274ce910e14a2e777
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-21 20:40:24 +0300

    add activeitem

--- src/components/molecules/CategoryItems/CategoryItems.js
+++ src/components/molecules/CategoryItems/CategoryItems.js
@@ -3,9 +3,24 @@ import { Component } from '../../../core/Component';
 import { eventEmmiter } from '../../../core/EventEmmiter';
 
 class CategoryItems extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeItem: null,
+    };
+  }
+
   static get observedAttributes() {
     return ['items'];
   }
+
+  setActiveCategory = (activeItem) => {
+    this.setState(() => {
+      return {
+        activeItem,
+      };
+    });
+  };
 
   setCategory = (evt) => {
     evt.preventDefault();
@@ -13,11 +28,14 @@ class CategoryItems extends Component {
       const id = evt.target.dataset.id;
       const items = JSON.parse(this.props.items);
       const selectedCategory = items.find((item) => item.id === Number(id));
+      this.setActiveCategory(selectedCategory);
       eventEmmiter.emit(APP_EVENTS.setCategory, { selectedCategory });
     }
   };
 
   componentDidMount() {
+    const items = JSON.parse(this.props.items);
+    this.setActiveCategory(items[0]);
     this.addEventListener('click', this.setCategory);
   }
 
@@ -27,15 +45,19 @@ class CategoryItems extends Component {
 
   render() {
     const items = JSON.parse(this.props.items);
+    const { activeItem } = this.state;
 
     return `
         <ul class="navbar-nav">
             ${items
               .slice(0, 5)
               .map((item) => {
+                const isActive = activeItem?.id === item.id;
                 return `
                 <li class="nav-item">
-                    <a class="nav-link" href="#" data-id="${item.id}">${item.name}</a>
+                    <a class="nav-link ${isActive ? 'active' : ''}" href="#" data-id="${item.id}">${
+                  item.name
+                }</a>
                 </li>
                 `;
               })

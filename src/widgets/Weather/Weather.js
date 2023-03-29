a3e9d9e6de498818d27fbbcdc67c6d90b4e7e23f
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-29 21:19:30 +0300

    new

--- src/widgets/Weather/Weather.js
+++ src/widgets/Weather/Weather.js
@@ -47,17 +47,80 @@ class Weather extends Component {
   render() {
     const { isLoading, weather } = this.state;
 
-    const weathercode = this.state.weather.current_weather;
-    console.log(weathercode);
+    const weatherStateMapIcon = {
+      0: '⭐',
+      1: '🌤️',
+      2: '🌥️',
+      3: '☁️',
+      45: '🌫️',
+      48: '❄️',
+      51: '🌦️',
+      53: '🌦️',
+      55: '🌦️',
+      56: '🌨️',
+      57: '🌨️',
+      61: '🌧️',
+      63: '🌧️',
+      65: '🌧️',
+      66: '🌧️',
+      67: '🌧️',
+      71: '❄️',
+      73: '❄️',
+      75: '❄️',
+      77: '❄️',
+      80: '🌧️',
+      81: '🌧️',
+      82: '🌧️',
+      85: '❄️',
+      86: '❄️',
+    };
+
+    const weatherStateMapText = {
+      0: 'Чистое небо',
+      1: 'Преимущественно ясно',
+      2: 'Переменная облачность',
+      3: 'Пасмурно',
+      45: 'Туман',
+      48: 'Изморозь',
+      51: 'Морось',
+      53: 'Морось',
+      55: 'Морось',
+      56: 'Ледяная морось',
+      57: 'Ледяная морось',
+      61: 'Дождь',
+      63: 'Дождь',
+      65: 'Дождь',
+      66: 'Ледяной дождь',
+      67: 'Ледяной дождь',
+      71: 'Снегопад:',
+      73: 'Снегопад:',
+      75: 'Снегопад:',
+      77: 'Снегопад:',
+      80: 'Ливневые дожди',
+      81: 'Ливневые дожди',
+      82: 'Ливневые дожди',
+      85: 'Снег',
+      86: 'Снег',
+    };
 
     return isLoading
-      ? `<div class="spinner-border text-primary" role="status">
-            <span class="visually-hidden">Loading...</span>
+      ? `
+        <div class="spinner-grow text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>`
       : ` 
-      <span class="placeholder col-12 bg-light">Погода в Гомеле:</span>
-        <a class="btn btn-primary disabled placeholder col-4">${weather?.current_weather?.temperature}</a>
-          <a class="btn btn-primary disabled placeholder col-4">${weather?.current_weather?.weathercode}</a>
+        <div class="card border-light mb-3" style="max-width: 18rem;">
+          <div class="card-header">Сейчас в Гомеле:</div>
+          <div class="card-body">
+            <h5 class="card-title fs-1" >🌡️ ${weather?.current_weather?.temperature} &#176;</h5>
+            <p class="card-text float-end fs-1">${
+              weatherStateMapIcon[weather?.current_weather?.weathercode]
+            }</p>
+            <p class="card-text fs-5 text-success-emphasis ">${
+              weatherStateMapText[weather?.current_weather?.weathercode]
+            }</p>
+          </div>
+        </div>
       `;
   }
 }

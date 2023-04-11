0c58734c78bf11c144c91187838c616be898aabc
VladiislavVolkov <kali.laskaa@gmail.com>
2023-04-11 20:38:09 +0300

    AuthService

---
+++ src/services/Auth.js
@@ -0,0 +1,28 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+
+import { cloudService } from './CloudService';
+
+class AuthService {
+  constructor() {
+    this.auth = getAuth(cloudService.app);
+  }
+
+  signIn(email, password) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  signUp(email, password) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  signOut() {
+    return signOut(this.auth);
+  }
+}
+
+export const authService = new AuthService();
